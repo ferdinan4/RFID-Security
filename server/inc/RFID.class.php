2a5195a0040c999d37578eadc429ab09eb1ccdbc
ferdinan4 <indurain_2@hotmail.com>
2016-12-03 02:56:09 +0100

    APIRest Commented

--- server/inc/RFID.class.php
+++ server/inc/RFID.class.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+*Class that include methods that are called when the someone make request agains the APIRest
+*
+**/
 class RFID {
     private $slim;
 
@@ -8,55 +12,98 @@ class RFID {
 
     private $params;
 
-    private $user;
-
+    /**
+    * RFID constructor
+    * @param $slim recieve an instance from the framework
+    **/
     public function __construct($slim) {
-        $this->config = json_decode(file_get_contents(__DIR__ . '/config.json'));
 
-        $this->db = db::getInstance($this->config->mysql_host, $this->config->mysql_name, $this->config->mysql_user, $this->config->mysql_pass);
+        // Decode JSON object from the DB config 
+        $this->config = json_decode(file_get_contents(__DIR__ . '/config.json')); 
+        //Obtain an instace from the DB
+        $this->db = db::getInstance($this->config->mysql_host, $this->config->mysql_name, $this->config->mysql_user, $this->config->mysql_pass); 
+
 
         $this->slim = $slim;
-        $this->params = json_decode($slim->request->getBody(), true);
-        $this->user = null;
+        //package the parameter of the HTTP request in an array
+        $this->params = json_decode($slim->request->getBody(), true); 
     }
 
-
+    /**
+    * Funtion to create an user   
+    **/
     public function createUser() {
-	$this->checkParams(array('user', 'pwd', 'name', 'surname'));
-	$this->db->insert("users", $this->params);
+        //Verify that at least we recieve the parameters 'user' 'pwd' 'name' and 'surname'
+	   $this->checkParams(array('user', 'pwd', 'name', 'surname'));
+	   $this->db->insert("users", $this->params);
     }
 
+    /**
+    * Funtion to create an Update an user 
+    * @param $id id from the user that we want to apply this function  
+    **/
     public function updateUser($id) {
-	$this->db->update("users", $this->params, "`id` = '{$id}'");
+	   $this->db->update("users", $this->params, "`id` = '{$id}'");
     }
 
+    /**
+    * Funtion to delete an User
+    * @param $id id from the user that we want to delete
+    **/
     public function deleteUser($id) {
         $this->db->delete("users", "`id` = '{$id}'");
     }
 
+    /**
+    * Funtion to create a Business
+    **/
     public function createBusiness() {
+       //Verify that at least we recieve the necessary parameters 
         $this->checkParams(array('name', 'lat', 'lon', 'radio'));
         $this->db->insert("business", $this->params);
     }
 
+    /**
+    * Funtion to update a Business
+    * @param $id id from the business that we want to update
+    **/
     public function updateBusiness($id) {
+        //Verify that at least we recieve the necessary parameters 
         $this->db->update("business", $this->params, "`id` = '{$id}'");
     }
 
+    /**
+    * Funtion to delete a Business
+    * @param $id id from the business that we want to delete
+    **/
     public function deleteBusiness($id) {
         $this->db->delete("business", "`id` = '{$id}'");
     }
 
+    /**
+    * Function to associate an user to a bussines
+    * @param $id id from the business
+    **/
     public function addUserToBusiness($id) {
 	$this->params['bid'] = $id;
         $this->checkParams(array('uid'));
         $this->db->insert("user_business", $this->params);
     }
 
+    /**
+    * Function to delete an user from a business
+    * @param $id business id
+    * @param $uid user id
+    **/
     public function deleteUserFromBusiness($id, $uid) {
         $this->db->delete("user_business", "`bid` = '{$id}' AND `uid` = '{$uid}'");
     }
 
+    /**
+    * Funtion that check if the geoposition of the mobile asociated to the "card id" is in allow range.
+    * @param $bid business id
+    * @param $cid card id
+    **/
     public function checkGPSLock($bid, $cid) {
 	$udata = $this->db->select("users", array("id", "lat", "lon"), "`card` = '{$cid}'");
 	if(count($udata)) {
@@ -72,6 +119,11 @@ class RFID {
 	$this->slim->halt(403, "");
     }
 
+    /**
+    * Function to check if this id card is associated to that bussiness
+    * @param $bid business id
+    * @param $cid card id
+    **/
     public function checkUserLock($bid, $cid) {
         $data = $this->db->select("users", array("id"), "`card` = '{$cid}'");
 	if(count($data)) {
@@ -83,6 +135,13 @@ class RFID {
 	$this->slim->halt(403, "");
     }
 
+    /**
+    * Function that calculate the distance between two positions given in metters
+    * @param $lat1 latitude of the one position
+    * @param $lon1 longitude of the one position
+    * @param $lat2 latitude of the two position
+    * @param $lon2 longitude of the two position
+    **/
     private function distance($lat1, $lon1, $lat2, $lon2) {
 	$theta = $lon1 - $lon2;
 	$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
@@ -93,6 +152,10 @@ class RFID {
 	return ($miles * 1609.344);
     }
 
+    /**
+    * Funtion that verify the params of the methods of the requests
+    * @param $params array of parameters that want to check
+    **/
     private function checkParams($params) {
         if (!is_array($this->params)) {
             $this->slim->halt(400, "This method requires parameters.");
--- server/public_html/index.php
+++ server/public_html/index.php
@@ -1,32 +1,88 @@
+
 <?php
+
 require __DIR__ . '/../inc/vendor/autoload.php';
 require __DIR__ . '/../inc/api_db.php';
 require __DIR__ . '/../inc/RFID.class.php';
 require __DIR__ . '/../inc/DefaultContentTypeMiddleware.class.php';
 
+/**
+*we create a instance of framework for developing the Web's  APIs
+**/
 $app = new \Slim\Slim();
 $app->add(new DefaultContentTypeMiddleware());
 
+/**
+*This object is which contents the methods that will call HTTP request
+**/
 $rfid = new RFID($app);
 
+/**
+*Return default message of the API
+**/
 $app->get('/', function() {
     echo "Hi there! I am using RFID-Security.";
 });
 
+/**
+*here we defined all request of the API
+**/
+
+/**
+*This is a POST request, for creating new users, and call 'createUser'  of the RFID class
+**/
 $app->post('/users/', array($rfid, 'createUser'));
+
+/**
+*This is a PUT request, which recieve an "id" from the user that we want to update, calling the method 'updateUser' from the RFID class
+**/
 $app->put('/users/:id/', array($rfid, 'updateUser'));
+
+/**
+*This is a DELETE request, which recieve an "id" from the user that we want to delete, calling the method 'deleteUser' from the RFID class
+**/
 $app->delete('/users/:id/', array($rfid, 'deleteUser'));
 
+/**
+*This is a POST request, for creating new Business, and call 'createBusiness'  of the RFID class
+**/
 $app->post('/business/', array($rfid, 'createBusiness'));
+
+/**
+*This is a PUT request, which recieve an "id" from the business that we want to update, calling the method 'updateBusiness' from the RFID class
+**/
 $app->put('/business/:id/', array($rfid, 'updateBusiness'));
+
+/**
+*This is a DELETE request, which recieve an "id" from the business that we want to delete, calling the method 'deleteBusiness' from the RFID class
+**/
 $app->delete('/business/:id/', array($rfid, 'deleteBusiness'));
 
+/**
+*This is a GET request, which recieve an "id" from the card and from the business asociated, calling the method 'checkGPSLock' from the RFID class
+*This 'checkGPSLock' check if the geoposition of the mobile asociated to the "card id" is allow range.
+**/
 $app->get('/business/:bid/check/gps/:cid/', array($rfid, 'checkGPSLock'));
+
+/**
+*This is a GET request, which recieve an "id" from the card that we want to delete, calling the method 'deleteUser' from the RFID class
+*This 'checkUserLock' check if this id card is associated to that bussiness
+**/
 $app->get('/business/:bid/check/:cid/', array($rfid, 'checkUserLock'));
 
+/**
+*This is a POST request, add an user to the business
+**/
 $app->post('/business/:id/employees', array($rfid, 'addUserToBusiness'));
+
+/**
+*This is a DELETE request, delete users associated to a business
+**/
 $app->delete('/business/:id/employees/:uid', array($rfid, 'deleteUserFromBusiness'));
 
+/**
+*Execute the APIRest
+**/
 $app->run();
 
 ?>
